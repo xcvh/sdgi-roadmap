eebf069eee3da2983e425f2b3cc9f7866501c440
xcvh <hi@jel.do>
2025-01-13 16:53:53 +0100

    added countries and article

---
+++ src/Article.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+const Article = () => {
+    return (
+        <div className="" id="sdg-roadmaps">
+            <h1 className="text-2xl font-bold underline">
+                Transforming Sustainable Development: Introducing the SDGiLevel
+                Departmental Roadmaps
+            </h1>
+
+            <h2 className="text-xl font-bold font-mono">
+                Unveiling the Institutional Capacity Phase Roadmaps
+            </h2>
+
+            <p className="">
+                The Institutional Capacity Phase has successfully culminated in
+                the creation of three pioneering SDGiLevel Departmental Roadmaps
+                at UdG, UNIRI, and KTU. These comprehensive blueprints are now
+                ready to guide our universities in recognizing, promoting, and
+                generating individual contributions to the Sustainable
+                Development Goals (SDGs).
+            </p>
+
+            <h3 className="text-lg font-bold">
+                What Are the SDGiLevel Departmental Roadmaps?
+            </h3>
+
+            <p className="">
+                The SDGiLevel Departmental Roadmaps are strategic plans tailored
+                to specific academic units such as departments, faculties, and
+                centers for entrepreneurship. Each roadmap outlines a program of
+                actionable measures, setting 4-6 specific goals, and detailing
+                the steps needed to achieve them. Designed as both management
+                and communication tools, these roadmaps will facilitate
+                strategic planning, decision-making, and articulate clear
+                programs of action.
+            </p>
+
+            <h3 className="">Achievements and Implementation</h3>
+
+            <p className="">
+                The development teams at each university have crafted these
+                roadmaps, guided by the Policy and Implementation Guidelines
+                (PR5). Each roadmap includes a selection of measures from the 30
+                proposed options and introduces new, innovative measures. Every
+                roadmap is set to implement at least four actionable steps,
+                ensuring tangible progress towards SDG contributions.
+            </p>
+
+            <h3 className="">Institutional Benefits</h3>
+
+            <p className="">
+                The implementation of these roadmaps offers a multitude of
+                benefits to academic units and the broader university community:
+            </p>
+
+            <ul className="">
+                <li className="">
+                    <strong className="">For Department Staff:</strong> The new
+                    policies and measures fostered by the SDGiLevel Departmental
+                    Roadmaps will support and recognize individual SDG
+                    contributions. This will promote an employee-centered
+                    culture, enhance collaboration, and boost morale and
+                    productivity.
+                </li>
+                <li className="">
+                    <strong className="">For Departments:</strong> By increasing
+                    their capacity and knowledge base regarding the SDG agenda,
+                    departments will improve their public image and foster a
+                    healthier, more productive work environment. The roadmaps
+                    provide a strategic framework for achieving these
+                    enhancements.
+                </li>
+                <li className="">
+                    <strong className="">
+                        For University Leaders and Stakeholders:
+                    </strong>{" "}
+                    The SDGiLevel Departmental Roadmaps serve as innovative
+                    models for SDG contribution, capturing the interest of
+                    university leaders and higher education experts. Their
+                    success paves the way for broader institutional adoption and
+                    further advancements in sustainable development.
+                </li>
+            </ul>
+
+            <h3 className="">A Vision for the Future</h3>
+
+            <p className="">
+                These innovative roadmaps bridge the gap between concept
+                development and institutional impact, translating previous
+                project outcomes into effective department-level plans. They
+                enhance the sustainability, visibility, and overall impact of
+                our SDG efforts.
+            </p>
+
+            <h3 className="">Join the Journey</h3>
+
+            <p className="">
+                As the first of their kind, these roadmaps are not just tools
+                but milestones in our commitment to sustainable development.
+                Their successful implementation will serve as a benchmark for
+                other departments and institutions to follow, demonstrating the
+                high transferability potential of our approach.
+            </p>
+
+            <p className="">
+                Embrace the future with us and witness how strategic planning
+                and individual empowerment can transform our universities into
+                leaders in sustainable development.
+            </p>
+        </div>
+    );
+};
+
+export default Article;
---
+++ src/MapChart.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { MapPin } from "lucide-react";
+
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Annotation,
+    ZoomableGroup,
+} from "react-simple-maps";
+import { PatternLines } from "@vx/pattern";
+
+const highlighted = ["LTU", "ESP", "HRV"];
+
+const MapChart = () => {
+    return (
+        <div className="border border-green-400 rounded-3xl">
+            <ComposableMap
+                projection="geoAzimuthalEqualArea"
+                projectionConfig={{
+                    rotate: [-10.0, -52.0, 0],
+                    center: [-5, -3],
+                    scale: 1100,
+                }}
+            >
+                <PatternLines
+                    id="lines"
+                    height={6}
+                    width={6}
+                    orientation={["diagonal"]}
+                    className="stroke-1 stroke-blue-800 bg-green-100"
+                />
+                <Geographies
+                    geography="/features.json"
+                    className="stroke-sky-500 stroke-[0.5px]"
+                >
+                    {({ geographies }) =>
+                        geographies.map((geo) => {
+                            const isHighlighted =
+                                highlighted.indexOf(geo.id) !== -1;
+                            return (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    fill={
+                                        isHighlighted
+                                            ? "url('#lines')"
+                                            : "#fef3c7"
+                                    }
+                                    onClick={() =>
+                                        console.log(geo.properties.name)
+                                    }
+                                />
+                            );
+                        })
+                    }
+                </Geographies>
+                <Annotation
+                    subject={[23.8813, 54.8985]} // Kaunas coordinates
+                    dx={0}
+                    dy={0}
+                >
+                    {/* Map pin for Kaunas */}
+                    <MapPin size={16} fill="#FF5533" color="#FFFFFF" />
+                </Annotation>
+
+                <Annotation
+                    subject={[2.8214, 41.9794]} // Girona coordinates
+                    dx={0}
+                    dy={0}
+                >
+                    {/* Map pin for Girona */}
+                    <MapPin size={16} fill="#FF5533" color="#FFFFFF" />
+                </Annotation>
+
+                <Annotation
+                    subject={[14.4422, 45.3271]} // Rijeka coordinates
+                    dx={0}
+                    dy={0}
+                >
+                    {/* Map pin for Rijeka */}
+                    <MapPin size={16} fill="#FF5533" color="#FFFFFF" />
+                </Annotation>
+            </ComposableMap>
+        </div>
+    );
+};
+
+export default MapChart;
